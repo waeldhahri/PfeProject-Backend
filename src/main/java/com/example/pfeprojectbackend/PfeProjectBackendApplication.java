2fc1de0b3214fa0be4193d4b1ec1ca2199f6f05a
waelepi <waeldhahri0@gmail.com>
2024-02-20 14:02:31 +0100

    Initial commit

---
+++ src/main/java/com/example/pfeprojectbackend/PfeProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pfeprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfeProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfeProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pfeprojectbackend/PfeProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pfeprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfeProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
