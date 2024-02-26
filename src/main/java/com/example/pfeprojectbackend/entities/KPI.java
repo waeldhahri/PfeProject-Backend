b168f714079ef6bdca34950fe01df86c941d1576
waeldhahri <waeldhahri3@gmail.com>
2024-02-26 14:20:27 +0100

    test commit with the new user

---
+++ src/main/java/com/example/pfeprojectbackend/entities/KPI.java
@@ -0,0 +1,22 @@
+package com.example.pfeprojectbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class KPI {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+
+    private String Points;
+}
